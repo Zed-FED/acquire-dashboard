306f8a802301e95ae89356cb7194390d8d432b0e
Zuber <zuber.kothiya@live.com>
2021-03-20 22:11:52 +0530

    Progress bar changes

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,50 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardComponent implements OnInit {
-  cards: object[];
-
 
   constructor() {
-    // this.cards = [
-    //   {
-    //     icon: 'activity',
-    //     title: 'Capacity',
-    //     heading: '150 GB',
-    //     noteIcon: 'refresh-cw',
-    //     noteText: 'Update Now',
-    //     bgClass: 'gradient-1'
-    //   },
-    //   {
-    //     icon: 'database',
-    //     title: 'Revenue',
-    //     heading: '$ 1,345',
-    //     noteIcon: 'calendar',
-    //     noteText: 'Last Day',
-    //     bgClass: 'gradient-2'
-    //   },
-    //   {
-    //     icon: 'heart',
-    //     title: 'Errors',
-    //     heading: '23',
-    //     noteIcon: 'clock',
-    //     noteText: 'Last Hour',
-    //     bgClass: 'gradient-3'
-    //   },
-    //   {
-    //     icon: 'star',
-    //     title: 'Followers',
-    //     heading: '+45K',
-    //     noteIcon: 'refresh-cw',
-    //     noteText: 'Update Now',
-    //     bgClass: 'gradient-4'
-    //   }
-    // ]
   }
 
   ngOnInit(): void {
