c3c5181d8de2774f2d1e0ca8b384d02c142cbd3b
Zuber <zuber.kothiya@live.com>
2021-03-21 14:13:09 +0530

    Server setup changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/acquire-dashboard'));
+app.use(express.static(__dirname + './dist/acquire-dashboard'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/acquire-dashboard/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/acquire-dashboard/'}),
+// );
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
