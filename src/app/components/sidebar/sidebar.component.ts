ddc1a0fa1bd6de86ed5489a64eea11209829e81e
Zuber <zuber.kothiya@live.com>
2021-03-21 11:46:05 +0530

    Responsive changes and build version

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  toggle: boolean = false;
+
+  clickEvent(event) {
+    this.toggle = !this.toggle
+  }
+
   mainNav: any[] = [
     {
       menuItem: 'Dashboard',
